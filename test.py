50c275add2b57e6becad8bd491223be8b00153e7
Loli <89763268+Loli1337@users.noreply.github.com>
2023-05-11 06:33:05 -0700

    Update test.py

--- test.py
+++ test.py
@@ -14,9 +14,3 @@ payload_json = json.dumps(payload)
 
 # Send the POST request to the webhook URL
 response = requests.post(webhook_url, data=payload_json, headers={"Content-Type": "application/json"})
-
-# Check the response status code
-if response.status_code == 204:
-    print("Message sent successfully!")
-else:
-    print(f"Failed to send message. Error {response.status_code}: {response.text}")
